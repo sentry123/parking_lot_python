6794f71b79e60889d1050e8acfd812616daa34fd
Abhisek Nanda <asip.nanda@hotmail.com>
2022-04-24 20:39:29 +0530

    "Changes to parking_lot_slot"

--- src/parking_lot_slot.py
+++ src/parking_lot_slot.py
@@ -8,24 +8,26 @@ from vehicle import Vehicle
 class ParkingLotSlot:
 
     parking_lot_vacancy = {
-        'Car_Slot' : 0,
-        'Bike_Slot' : 0,
-        'Truck_Slot' : 0
+        'Car': 0,
+        'Bike': 0,
+        'Truck': 0
     }
 
     parking_lot_vehicles = {}
 
     def __init__(self, car_slot=3, bike_slot=3, truck_slot=3):
-        self.parking_lot_vacancy['Car_Slot'] = car_slot
-        self.parking_lot_vacancy['Bike_Slot'] = bike_slot
-        self.parking_lot_vacancy['Truck_Slot'] = truck_slot
+        self.parking_lot_vacancy['Car'] = car_slot
+        self.parking_lot_vacancy['Bike'] = bike_slot
+        self.parking_lot_vacancy['Truck'] = truck_slot
 
-    def park_a_vehicle(self, vehicle:Vehicle):
-        if Vehicle.validate_type(vehicle.vehicle_type):
+    def park_a_vehicle(self, vehicle: Vehicle):
+
+        if Vehicle.validate_type(vehicle.vehicle_type) and self.parking_lot_vacancy[vehicle.vehicle_type] > 0:
             self.parking_lot_vehicles[vehicle.registration_number] = vehicle
+            self.parking_lot_vacancy[vehicle.vehicle_type] -= 1
             print(f"Vehicle : {vehicle.registration_number} Parked Successfully ")
         else:
-            print("Vehicle type not compatible for this parking structure")
+            print("No suitable vacancy")
 
     def unpark_a_vehicle(self, registration_number: str):
         if registration_number in self.parking_lot_vehicles:
